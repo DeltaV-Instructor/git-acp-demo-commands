06da5647f18756c33afc0ae7c583cff8100408c8
tektechnologies <info@tektechnologies.com>
2023-06-27 10:51:52 -0500

    add formatting

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
-'use strict';
+"use strict";
 
-
-console.log('Hello World, how are you doing?');
-
+console.log("Hello World, how are you doing?");
 
 console.log(5 + 5 + 5);
 
-
 let five = 5;
 let ten = 10;
-//                       + connects text with the math. 
-console.log((five + ten) + " = 15");
+//                       + connects text with the math.
+console.log(five + ten + " = 15");
