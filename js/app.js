6db9220be3018a12fde5207650e8130843b15540
tektechnologies <info@tektechnologies.com>
2023-06-27 10:49:09 -0500

    add js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,9 @@ console.log('Hello World, how are you doing?');
 
 
 console.log(5 + 5 + 5);
+
+
+let five = 5;
+let ten = 10;
+//                       + connects text with the math. 
+console.log((five + ten) + " = 15");
