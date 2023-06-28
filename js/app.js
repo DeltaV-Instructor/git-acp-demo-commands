d04d9e1a1e02451190c182225d0be042cf63978e
tektechnologies <info@tektechnologies.com>
2023-06-28 14:12:38 -0500

    add link to js in the html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 "use strict";
 
-console.log("Hello World, how are you doing?");
-
-console.log(5 + 5 + 5);
-
-let five = 5;
-let ten = 10;
-//                       + connects text with the math.
-console.log(five + ten + " = 15");
+// console.log("Hello World, how are you doing?");
+// console.log(5 + 5 + 5);
+// let five = 5;
+// let ten = 10;
+// + connects text with the math.
+// console.log(five + ten + " = 15");
